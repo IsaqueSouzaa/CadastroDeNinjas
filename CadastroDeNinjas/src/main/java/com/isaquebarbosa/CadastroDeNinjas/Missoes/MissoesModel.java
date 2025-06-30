8a2218a6f83d1f1560a040efa8d77dfff554bdc2
IsaqueSouzaa <isaquebarbosa463@gmail.com>
2025-06-30 02:22:36 -0300

    Alterei a arquitetura do codigo com Lombok

--- CadastroDeNinjas/src/main/java/com/isaquebarbosa/CadastroDeNinjas/Missoes/MissoesModel.java
+++ CadastroDeNinjas/src/main/java/com/isaquebarbosa/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,9 +2,15 @@ package com.isaquebarbosa.CadastroDeNinjas.Missoes;
 
 import com.isaquebarbosa.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table (name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
     @Id
